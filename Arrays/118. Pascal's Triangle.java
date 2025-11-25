91e02f81faf20372b33357bafc37475f87ca133a
Lalit Kumar <shahulalit5@gmail.com>
2025-11-25 17:24:06 +0530

    pascal triangle 1

---
+++ Arrays/118. Pascal's Triangle.java
@@ -0,0 +1,24 @@
+public class 118. Pascal's Triangle {
+  class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>>ans = new ArrayList<>();
+
+        for(int i = 1; i<= numRows; i++){
+            ans.add(generateRow(i));
+        }
+        return ans;
+    }
+    public List<Integer>generateRow(int row){
+        List<Integer>ansRow = new ArrayList<>();
+        long ans = 1;
+        ansRow.add(1);
+
+        for(int col = 1; col<row; col++){
+            ans =  ans * (row - col);
+            ans = ans/col;
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+}
+}
