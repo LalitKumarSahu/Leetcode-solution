fa8c3f1aeeb98c108603cbd27dc2ee8b207d13db
Lalit Kumar <shahulalit5@gmail.com>
2025-11-26 22:29:09 +0530

    added count inversion soln

--- Arrays/CountInversion.java
+++ Arrays/CountInversion.java
@@ -12,5 +12,64 @@ public class CountInversion {
     }
 
     // optimal soln
-   }
+   public long numberOfInversions2(int[] nums) {
+    int n = nums.length;
+    return mergeSort(nums, 0, n - 1);
+}
+
+public long mergeSort(int arr[], int low, int high) {
+
+    if (low >= high) return 0;
+
+    int mid = low + (high - low) / 2;
+
+    long count = 0;
+
+    count += mergeSort(arr, low, mid);
+    count += mergeSort(arr, mid + 1, high);
+
+    count += countNumberOfInversion(arr, low, mid, high);
+
+    merge(arr, low, mid, high);
+
+    return count;
+}
+
+public long countNumberOfInversion(int arr[], int low, int mid, int high) {
+
+    long count = 0;
+    int right = mid + 1;
+
+    for (int i = low; i <= mid; i++) {
+
+        while (right <= high && arr[i] > arr[right]) {
+            right++;
+        }
+
+        count += (right - (mid + 1));
+    }
+
+    return count;
+}
+
+public void merge(int arr[], int low, int mid, int high) {
+
+    int temp[] = new int[high - low + 1];
+    int left = low, right = mid + 1, idx = 0;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp[idx++] = arr[left++];
+        } else {
+            temp[idx++] = arr[right++];
+        }
+    }
+
+    while (left <= mid) temp[idx++] = arr[left++];
+    while (right <= high) temp[idx++] = arr[right++];
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
 }
