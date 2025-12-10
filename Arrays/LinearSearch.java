14815cac33e9415ba48a69acb0fd6e110ac5e475
Lalit Kumar <shahulalit5@gmail.com>
2025-12-10 22:52:54 +0530

    added lenear search

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,12 +1,14 @@
-public class LinearSearch {
-  public static int linearSearch(int arr[], int k){
-
-    int n = arr.length;
-    for(int i= 0; i<n; i++){
-      if(arr[i] == k){
-        return i;
-      }
+class Solution {
+    public int search(int arr[], int x) {
+        // code here
+        
+        int n = arr.length;
+        for(int i = 0; i<n; i++){
+            if(arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
+        
     }
-    return -1;
-  }
 }
