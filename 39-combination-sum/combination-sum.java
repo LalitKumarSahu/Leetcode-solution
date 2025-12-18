6ac62777938cc9468878c2be934db114ea379849
Lalit Kumar <shahulalit5@gmail.com>
2025-12-18 21:14:56 +0530

    added combination sum II

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,37 +1,37 @@
-// class Solution {
-//     //by using pick or no pick
-//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-//       List<List<Integer>> res = new ArrayList<>();
-//      List<Integer> curr = new ArrayList<>();
-//      rec(candidates, target, 0, curr, res);
-//      return res;
+class Solution {
+    //by using pick or no pick
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+      List<List<Integer>> res = new ArrayList<>();
+     List<Integer> curr = new ArrayList<>();
+     rec(candidates, target, 0, curr, res);
+     return res;
 
-//     }
-//     public static void rec(int[] candidates, int target,int idx,List<Integer> curr, List<List<Integer>>res){
-//           // base case
-//           if(idx == candidates.length){
-//             if(target == 0){
-//                 res.add(new ArrayList<>(curr));
+    }
+    public static void rec(int[] candidates, int target,int idx,List<Integer> curr, List<List<Integer>>res){
+          // base case
+          if(idx == candidates.length){
+            if(target == 0){
+                res.add(new ArrayList<>(curr));
 
-//             }
-//             return;
-//           }
+            }
+            return;
+          }
 
-//           //include
-//           if(candidates[idx] <= target){
-//             curr.add(candidates[idx]);
-//             rec(candidates, target-candidates[idx], idx, curr, res);
-//             // backtrack
-//             curr.remove(curr.size()-1);
-//           }
-//           //exclude
-//            rec(candidates, target, idx+1, curr, res);
-//     }
-// }
+          //include
+          if(candidates[idx] <= target){
+            curr.add(candidates[idx]);
+            rec(candidates, target-candidates[idx], idx, curr, res);
+            // backtrack
+            curr.remove(curr.size()-1);
+          }
+          //exclude
+           rec(candidates, target, idx+1, curr, res);
+    }
+}
 
 
 
-// -------------------------------------------------------->
+// --------------2^n * k------------------------------------------>
 class Solution {
     //by using pick or no pick
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
---
+++ rec & dp/CombinationSumII.java
@@ -0,0 +1,35 @@
+ import java.util.*;
+public class CombinationSumII {
+ 
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        TargetSum2(0, candidates, new ArrayList<>(), res, target); // data structure initiall empty
+        return res;
+    }
+    
+    public static void TargetSum2(int idx, int[] nums, List<Integer> curr, List<List<Integer>> res, int target) {
+        // Base case
+        if (target == 0) {
+            res.add(new ArrayList(curr));
+            return;
+        }
+        
+        for (int i = idx; i < nums.length; i++) { // i to n combination
+
+            // Skip duplicates
+            if (  (i == idx || nums[i] != nums[i - 1]) && nums[i] <= target) { // pehla idx, uniq comb, less then t
+                 curr.add(nums[i]); // Include current number
+            // Recur with next index and reduced target
+            TargetSum2(i + 1, nums, curr, res, target - nums[i]);
+            curr.remove(curr.size() - 1); // Backtrack
+            }
+            
+        
+        }
+    }
+}
+}
