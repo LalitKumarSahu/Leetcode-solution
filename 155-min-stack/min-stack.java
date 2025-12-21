fdb11675282c131379f5509c8b60990feec38214
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2025-12-21 21:48:49 +0530

    Time: 5 ms (76.60%) | Memory: 47.2 MB (19.11%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,34 +1,65 @@
 import java.util.Stack;
+import java.util.Stack;
 
 class MinStack {
 
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
+    Stack<int[]> stack;
 
     public MinStack() {
         stack = new Stack<>();
-        minStack = new Stack<>();
     }
 
     public void push(int val) {
-        stack.push(val);
-        if (minStack.isEmpty() || val <= minStack.peek()) {
-            minStack.push(val);
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int currentMin = Math.min(val, stack.peek()[1]);
+            stack.push(new int[]{val, currentMin});
         }
     }
 
     public void pop() {
-        if (stack.peek().equals(minStack.peek())) {
-            minStack.pop();
-        }
         stack.pop();
     }
 
     public int top() {
-        return stack.peek();
+        return stack.peek()[0];
     }
 
     public int getMin() {
-        return minStack.peek();
+        return stack.peek()[1];
     }
 }
+
+// class MinStack {
+
+//     Stack<Integer> stack;
+//     Stack<Integer> minStack;
+
+//     public MinStack() {
+//         stack = new Stack<>();
+//         minStack = new Stack<>();
+//     }
+
+//     public void push(int val) {
+//         stack.push(val);
+//         if (minStack.isEmpty() || val <= minStack.peek()) {
+//             minStack.push(val);
+//         }
+//     }
+
+//     public void pop() {
+//         if (stack.peek().equals(minStack.peek())) {
+//             minStack.pop();
+//         }
+//         stack.pop();
+//     }
+
+//     public int top() {
+//         return stack.peek();
+//     }
+
+//     public int getMin() {
+//         return minStack.peek();
+//     }
+// }
