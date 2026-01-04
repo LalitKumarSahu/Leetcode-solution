a27c0f3955588a0b619c89e4bab298ed8a7b4701
Lalit Kumar <shahulalit5@gmail.com>
2026-01-04 10:31:15 +0530

    added jump game 1

---
+++ Greedy/JumpGame1.java
@@ -0,0 +1,14 @@
+public class JumpGame1 {
+   public boolean canJump(int[] nums) {
+        int currMax = 0;  // The farthest index we can reach
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > currMax) return false; // If we can't reach index 'i', return false
+
+            currMax = Math.max(currMax, i + nums[i]); // Update max reachable index
+
+            if (currMax >= nums.length - 1) return true; // If we can reach the last index, return true
+        }
+        return false; // If loop completes, return false
+    }
+}
