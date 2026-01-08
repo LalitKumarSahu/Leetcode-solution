86cfc209c8a45609c473f218e9cf03e283075622
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-01-08 08:33:57 +0530

    Time: 4 ms (26.22%) | Memory: 47.6 MB (54.08%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,11 +1,45 @@
 class Solution {
-    public int candy(int[] ratings) {
+    // tc:O(3N), sc:O(2N)
+    // public int candy(int[] ratings) {
+    //     int n = ratings.length;
+    //     int left [] = new int[n];
+    //     int right [] = new int[n];
+
+    //     left[0] = 1;
+    //     right[n-1] = 1;
+
+    //     for(int i = 1; i<n; i++){
+    //         if(ratings[i] > ratings[i-1]){
+    //             left[i] = left[i-1] + 1;
+    //         }else{
+    //             left[i] = 1;
+    //         }
+    //     }
+
+    //     for(int i = n-2; i>= 0; i--){
+    //         if(ratings[i] > ratings[i+1]){
+    //             right[i] = right[i+1] + 1;
+    //         }else{
+    //             right[i] = 1;
+    //         }
+    //     }
+    //      int sum = 0;
+    //     for(int i = 0; i<n; i++){
+    //         sum = sum + Math.max(left[i], right[i]);
+    //     }
+    //     return sum;
+
+        
+    // }
+
+
+    // m-2 optimize the sc
+     public int candy(int[] ratings) {
         int n = ratings.length;
         int left [] = new int[n];
-        int right [] = new int[n];
-
+       
         left[0] = 1;
-        right[n-1] = 1;
+        
 
         for(int i = 1; i<n; i++){
             if(ratings[i] > ratings[i-1]){
@@ -14,18 +48,21 @@ class Solution {
                 left[i] = 1;
             }
         }
-
+          int curr = 1, right = 1;
+           int sum = 0;
+           sum = Math.max(left[n-1], 1);
         for(int i = n-2; i>= 0; i--){
             if(ratings[i] > ratings[i+1]){
-                right[i] = right[i+1] + 1;
+                curr = right + 1;
+                right = curr;
             }else{
-                right[i] = 1;
+               curr = 1;
+               right = 1;
             }
+            sum = sum + Math.max(left[i], curr);
+
         }
-         int sum = 0;
-        for(int i = 0; i<n; i++){
-            sum = sum + Math.max(left[i], right[i]);
-        }
+        
         return sum;
 
         
