041d1ef6a27ee55252ba81c9a8366acb556d69ed
Lalit Kumar <shahulalit5@gmail.com>
2026-01-08 08:35:40 +0530

    added candy with optmize sc

--- Greedy/Candy.java
+++ Greedy/Candy.java
@@ -30,5 +30,40 @@ public class Candy {
 
         
     }
+
+    // optimize sc in O(N)   
+    public int candy2(int[] ratings) {
+        int n = ratings.length;
+        int left [] = new int[n];
+       
+        left[0] = 1;
+        
+
+        for(int i = 1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
+            }else{
+                left[i] = 1;
+            }
+        }
+          int curr = 1, right = 1;
+           int sum = 0;
+           sum = Math.max(left[n-1], 1);
+        for(int i = n-2; i>= 0; i--){
+            if(ratings[i] > ratings[i+1]){
+                curr = right + 1;
+                right = curr;
+            }else{
+               curr = 1;
+            }
+            sum = sum + Math.max(left[i], curr);
+
+        }
+        
+        return sum;
+
+        
+    }
+
 }
 
