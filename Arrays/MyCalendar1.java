9265060416d994184b9ae95e8a55938d3bb32fa8
Lalit Kumar <shahulalit5@gmail.com>
2026-01-08 11:09:27 +0530

    added my calendar I

---
+++ Arrays/MyCalendar1.java
@@ -0,0 +1,24 @@
+//m-1 Brute force 
+// check if non-overlaping add into the list and return true
+//else if there is overlaping return false;
+
+import java.util.*;
+class MyCalendar {
+    //(Key, val)
+    TreeMap<Integer, Integer>map;
+    public MyCalendar() {
+        map = new TreeMap<>(); 
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        Integer lastEnd = map.lowerKey(endTime);
+        // overlap exist
+        if(lastEnd != null && startTime <= map.get(lastEnd)-1){
+            return false;
+        } 
+
+        // if there is no overlap then add into the map (blind insertion)
+        map.put(startTime, endTime);
+        return true;
+    }
+}
