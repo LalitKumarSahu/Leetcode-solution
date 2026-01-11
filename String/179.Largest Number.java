64589d65982e46fb6b3dac384dcacd3cdc4204dd
Lalit Kumar <shahulalit5@gmail.com>
2026-01-11 11:26:55 +0530

    added 179 Largest number

--- String/179.Largest Number.java
+++ String/179.Largest Number.java
@@ -1,3 +1,8 @@
+// we will sort not based on inc or dec 
+// we wwill sort based on two comparesion
+// for eg - 23, 9 = 239 or 923
+// i = 0 j = i+1 to n compare and swap i = 1 and j = i+1 and so on
+import java.util.*;
 class Solution {
     public String largestNumber(int[] nums) {
         int n = nums.length;
