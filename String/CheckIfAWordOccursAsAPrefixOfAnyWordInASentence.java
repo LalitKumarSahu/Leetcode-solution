18440824fb9bb9cbf1aa97882f0f6af0603f9b00
Lalit Kumar <shahulalit5@gmail.com>
2026-01-12 11:05:40 +0530

    1455. m-2 without split and substring

--- String/CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.java
+++ String/CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.java
@@ -16,4 +16,46 @@ public class CheckIfAWordOccursAsAPrefixOfAnyWordInASentence {
         return -1;
     }
 }
+
+// m-2 without using split and substring
+class Solution2 {
+    public int isPrefixOfWord2(String sentence, String searchWord) {
+        int n = sentence.length();
+        int m = searchWord.length();
+        int i = 0;
+        int j = 0;
+        int idx = 1;
+
+        while (i < n) {
+
+            // skip leading spaces
+            while (i < n && sentence.charAt(i) == ' ') {
+                i++;
+            }
+
+            // compare word with searchWord
+            j = 0;
+            int start = i;
+            while (i < n && j < m && sentence.charAt(i) == searchWord.charAt(j)) {
+                i++;
+                j++;
+            }
+
+            // if full searchWord matched
+            if (j == m) {
+                return idx;
+            }
+
+            // move to end of current word
+            while (i < n && sentence.charAt(i) != ' ') {
+                i++;
+            }
+
+            // move to next word
+            idx++;
+        }
+
+        return -1;
+    }
+}
 }
