7977bc2a6bd2ac72b326d885711b553f92743a14
Lalit Kumar <shahulalit5@gmail.com>
2026-01-14 14:54:30 +0530

    added two sum usin map

--- heap&hashing/TwoSum.java
+++ heap&hashing/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class TwoSum {
   class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -13,5 +15,23 @@ public class TwoSum {
         
     }
 }
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+        int res[] = new int[2];
+        for(int i= 0; i<nums.length; i++){
+            int partner = target - nums[i];
+            if(map.containsKey(partner)){
+                res[0] = i;
+                res[1] = map.get(partner);
+                return res;
+             }
+             map.put(nums[i], i);
+            }
+        
+      return null;
+        
+    }
+}
+}
 
