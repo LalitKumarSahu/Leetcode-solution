cac3df91ed7fbe3cf9511c2d8c3b6e3baa5b6ccb
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-01-22 13:28:47 +0530

    Time: 3 ms (88.75%) | Memory: 47.9 MB (11.95%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,105 +1,35 @@
 class Solution {
-    // tc:O(3N), sc:O(2N)
-    // public int candy(int[] ratings) {
-    //     int n = ratings.length;
-    //     int left [] = new int[n];
-    //     int right [] = new int[n];
-
-    //     left[0] = 1;
-    //     right[n-1] = 1;
-
-    //     for(int i = 1; i<n; i++){
-    //         if(ratings[i] > ratings[i-1]){
-    //             left[i] = left[i-1] + 1;
-    //         }else{
-    //             left[i] = 1;
-    //         }
-    //     }
-
-    //     for(int i = n-2; i>= 0; i--){
-    //         if(ratings[i] > ratings[i+1]){
-    //             right[i] = right[i+1] + 1;
-    //         }else{
-    //             right[i] = 1;
-    //         }
-    //     }
-    //      int sum = 0;
-    //     for(int i = 0; i<n; i++){
-    //         sum = sum + Math.max(left[i], right[i]);
-    //     }
-    //     return sum;
-
-        
-    // }
-
-
-    // m-2 optimize the sc
-    //  public int candy(int[] ratings) {
-    //     int n = ratings.length;
-    //     int left [] = new int[n];
-       
-    //     left[0] = 1;
-        
-
-    //     for(int i = 1; i<n; i++){
-    //         if(ratings[i] > ratings[i-1]){
-    //             left[i] = left[i-1] + 1;
-    //         }else{
-    //             left[i] = 1;
-    //         }
-    //     }
-    //       int curr = 1, right = 1;
-    //        int sum = 0;
-    //        sum = Math.max(left[n-1], 1);
-    //     for(int i = n-2; i>= 0; i--){
-    //         if(ratings[i] > ratings[i+1]){
-    //             curr = right + 1;
-    //             right = curr;
-    //         }else{
-    //            curr = 1;
-    //            right = 1;
-    //         }
-    //         sum = sum + Math.max(left[i], curr);
-
-    //     }
-        
-    //     return sum;
-
-        
-    // }
-
-    // m-3 optimal soln
-         public int candy(int[] ratings) {
+    public int candy(int[] ratings) {
          int n = ratings.length;
-         int i = 1;
-         int sum = 1;
-
-         while( i < n){
-            // flat slope
-            if(ratings[i] == ratings[i-1]){
-                sum = sum + 1;
-                i++;
-                continue;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        
+        left[0] = 1;
+        right[n-1] = 1;
+        
+        // Left to Right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
             }
-            // inc slope
-            int peak = 1;
-            while(i < n && ratings[i] > ratings[i-1]){
-                peak += 1;
-                sum += peak;
-                i++;
+        }
+        
+        // Right to Left  ✅ FIXED
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {   // ✅ correct
+                right[i] = right[i + 1] + 1;
+            } else {
+                right[i] = 1;
             }
-            // dec slope
-            int down = 1;
-             while(i < n && ratings[i] < ratings[i-1]){
-                sum += down;
-                i++;
-                down += 1;
-            }
-            // peak adjustment
-            if(down > peak){
-                sum += down - peak;
-            }
-         }
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.max(left[i], right[i]);
+        }
+        
         return sum;
     }
 }
