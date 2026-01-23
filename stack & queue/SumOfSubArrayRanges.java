d588083f84ead969b2593f72c2ae2a9479fc4c25
Lalit Kumar <shahulalit5@gmail.com>
2026-01-23 13:14:01 +0530

    added better soln of the sum of subaray ranges

--- stack & queue/SumOfSubArrayRanges.java
+++ stack & queue/SumOfSubArrayRanges.java
@@ -23,4 +23,21 @@ public class SumOfSubArrayRanges {
     }
 }
 
+// better soln
+  public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+                int min = Integer.MAX_VALUE;
+                int max = Integer.MIN_VALUE;
+            for (int j = i; j < n; j++) {
+                    min = Math.min(min, nums[j]);
+                    max = Math.max(max, nums[j]);
+                    ans += (max - min);
+            }
+        }
+        return ans;
+    }
+
 }
