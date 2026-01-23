4e7f5af420e94c5fbee8d7d179f05bcb6f250031
Lalit Kumar <shahulalit5@gmail.com>
2026-01-23 13:19:51 +0530

    added optimal soln of sum of subarray ranges

--- stack & queue/SumOfSubArrayRanges.java
+++ stack & queue/SumOfSubArrayRanges.java
@@ -24,7 +24,7 @@ public class SumOfSubArrayRanges {
 }
 
 // better soln
-  public long subArrayRanges(int[] nums) {
+  public long subArrayRanges2(int[] nums) {
         int n = nums.length;
         long ans = 0;
 
@@ -39,5 +39,89 @@ public class SumOfSubArrayRanges {
         }
         return ans;
     }
+
+    // optimal soln
+   
+    public long subArrayRanges3(int[] nums) {
+        int n = nums.length;
+
+        long sumMin = getSumSubarrayMins(nums);
+        long sumMax = getSumSubarrayMax(nums);
+
+        return sumMax - sumMin;
+    }
+
+    // 🔹 Sum of Subarray Minimums
+    private long getSumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int[] ple = new int[n];
+        int[] nle = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        // PLE
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            ple[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        // NLE
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            nle[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - ple[i];
+            long right = nle[i] - i;
+            sum += (long) arr[i] * left * right;
+        }
+        return sum;
+    }
+
+    // 🔹 Sum of Subarray Maximums
+    private long getSumSubarrayMax(int[] arr) {
+        int n = arr.length;
+        int[] pge = new int[n]; // previous greater
+        int[] nge = new int[n]; // next greater or equal
+        Stack<Integer> st = new Stack<>();
+
+        // PGE
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+                st.pop();
+            }
+            pge[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        // NGE
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                st.pop();
+            }
+            nge[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - pge[i];
+            long right = nge[i] - i;
+            sum += (long) arr[i] * left * right;
+        }
+        return sum;
+    }
+
 
 }
