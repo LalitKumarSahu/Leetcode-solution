1d0487c856cefc1e54a29a1cd7d808a06831fed8
Lalit Kumar <shahulalit5@gmail.com>
2026-01-23 13:10:11 +0530

    added brute approach of the sum of subaray ranges

--- stack & queue/SumOFSubArrayMin.java
+++ stack & queue/SumOFSubArrayMin.java
@@ -22,7 +22,7 @@ public class SumOFSubArrayMin {
  public int sumSubarrayMins2(int[] arr) {
     int n = arr.length;
     long ans = 0;
-    int mod = (int)(1e9 + 7);
+    int  mod = (int)(1e9 + 7);
 
     for (int i = 0; i < n; i++) {
         int min = Integer.MAX_VALUE;
@@ -37,3 +37,49 @@ public class SumOFSubArrayMin {
 }
 
  // m-3 optimal soln
+// Har element arr[i] ko hum ye count karte hain ki wo kitne subarrays me minimum ban raha hai.
+//formula -> Contribution of arr[i] = arr[i] * left * right
+
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int MOD = 1_000_000_007;
+
+        int[] ple = new int[n]; // previous less element index
+        int[] nle = new int[n]; // next less or equal element index
+
+        Stack<Integer> st = new Stack<>();
+
+        // 🔹 PLE
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            ple[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        // 🔹 NLE
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            nle[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        long ans = 0;
+
+        // 🔹 Contribution calculation
+        for (int i = 0; i < n; i++) {
+            long left = i - ple[i];
+            long right = nle[i] - i;
+
+            ans = (ans + (arr[i] * left % MOD) * right % MOD) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
---
+++ stack & queue/SumOfSubArrayRanges.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class SumOfSubArrayRanges {
+  class Solution {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int min = Integer.MAX_VALUE;
+                int max = Integer.MIN_VALUE;
+
+                for (int k = i; k <= j; k++) {
+                    min = Math.min(min, nums[k]);
+                    max = Math.max(max, nums[k]);
+                }
+
+                ans += (max - min);
+            }
+        }
+        return ans;
+    }
+}
+
+}
