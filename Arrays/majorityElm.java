ce54f2adc7cbac8b9b46e651d6ec14ced7f05fa1
Lalit Kumar <shahulalit5@gmail.com>
2026-02-04 09:51:05 +0530

    majority elem method 3

--- Arrays/majorityElm.java
+++ Arrays/majorityElm.java
@@ -36,5 +36,25 @@ class Solution2 {
     }
 }
 
+// m-3
+class Solution3 {
+    public int majorityElement(int[] nums) {
+       int n = nums.length;
+       int candidate = nums[0];
+       int vote = 1;
+
+       for(int i = 1; i<n; i++){
+         if(vote == 0){
+            candidate = nums[i];
+            vote = 1;
+         }else if(candidate == nums[i]){
+            vote++;
+         }else{
+            vote--;
+         }
+       }
+       return candidate;
+    }
+}
 
 }
