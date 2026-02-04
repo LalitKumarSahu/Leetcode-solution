4836866da50c8c68bdb542214b2969389fdae727
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-02-04 16:15:57 +0530

    Time: 180 ms (52.04%) | Memory: 47.4 MB (27.08%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,63 @@
+class Solution {
+    // public int countPalindromicSubsequence(String s) {
+    //     HashSet<Character> set = new HashSet<>();
+
+    //     for (char ch : s.toCharArray()) { // O(N)
+    //         set.add(ch);
+    //     }
+    //     int count = 0;
+    //     for (char ch : set) { // 26
+    //         int first = -1;
+    //         int last = -1;
+    //         for (int i = 0; i < s.length(); i++) { //N
+    //             if (ch == s.charAt(i)) {
+    //                 if (first == -1) { // its a first occurence
+    //                     first = i;
+    //                 }
+    //                 last = i;
+
+    //             }
+
+    //         }
+    //         if (first == last)
+    //             continue; // only one char
+
+    //         HashSet<Character> set1 = new HashSet<>();
+    //         for (int i = first + 1; i < last; i++) {
+    //             set1.add(s.charAt(i));
+    //         }
+    //         count += set1.size();
+    //     }
+    //     return count;
+
+    // }
+
+     public int countPalindromicSubsequence(String s) {
+       HashMap<Character,Integer>firstOcc = new HashMap<>();
+       HashMap<Character,Integer>lastOcc = new HashMap<>();
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(!firstOcc.containsKey(ch)){
+                firstOcc.put(ch, i);
+            }
+            lastOcc.put(ch, i);
+        }
+        
+        int count = 0;
+        for (char ch : firstOcc.keySet()) { // 26
+            int first = firstOcc.get(ch);
+            int last = lastOcc.get(ch);
+           
+            if (first == last)
+                continue; // only one char
+
+            HashSet<Character> set1 = new HashSet<>();
+            for (int i = first + 1; i < last; i++) {
+                set1.add(s.charAt(i));
+            }
+            count += set1.size();
+        }
+        return count;
+
+    }
+}
