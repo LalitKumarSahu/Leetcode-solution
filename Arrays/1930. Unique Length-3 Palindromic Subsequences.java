f5878b4b0c630a1f7f0957a29e69cefb37af8f70
Lalit Kumar <shahulalit5@gmail.com>
2026-02-04 16:17:56 +0530

    unique Length -3 palindromic

--- Arrays/1930. Unique Length-3 Palindromic Subsequences.java
+++ Arrays/1930. Unique Length-3 Palindromic Subsequences.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int countPalindromicSubsequence(String s) {
         HashSet<Character>set = new HashSet<>();
@@ -29,6 +30,36 @@ class Solution {
         }
        return count;
 
+
+    }
+   
+    // m-2
+      public int countPalindromicSubsequence2(String s) {
+       HashMap<Character,Integer>firstOcc = new HashMap<>();
+       HashMap<Character,Integer>lastOcc = new HashMap<>();
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(!firstOcc.containsKey(ch)){
+                firstOcc.put(ch, i);
+            }
+            lastOcc.put(ch, i);
+        }
+        
+        int count = 0;
+        for (char ch : firstOcc.keySet()) { // 26
+            int first = firstOcc.get(ch);
+            int last = lastOcc.get(ch);
+           
+            if (first == last)
+                continue; // only one char
+
+            HashSet<Character> set1 = new HashSet<>();
+            for (int i = first + 1; i < last; i++) {
+                set1.add(s.charAt(i));
+            }
+            count += set1.size();
+        }
+        return count;
 
     }
 }
