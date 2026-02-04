97763b64af527549281f9ab8fc717390d2a9e87d
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-02-04 12:55:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (88.15%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int maxVal = 0;
+        int chunks = 0;
+
+        for(int i=0; i<arr.length; i++){
+            maxVal = Math.max(maxVal, arr[i]);
+            if(maxVal == i){
+                chunks++;
+            }
+        }
+        return chunks;
+        
+    }
+}
