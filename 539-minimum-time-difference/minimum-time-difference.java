5a6aa86b1fd1f155d45523dc013b7d2627c37e2e
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-02-05 15:23:48 +0530

    Time: 2 ms (99.36%) | Memory: 47.9 MB (71.92%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -0,0 +1,71 @@
+// class Solution {
+//     //m-1 covert into min, sort, then find adjacent diff
+//     public int findMinDifference(List<String> timePoints) {
+//         // convert input to minutes
+//         int[] minutes = new int[timePoints.size()];
+//         for (int i = 0; i < timePoints.size(); i++) {
+//             String time = timePoints.get(i);
+//             int h = Integer.parseInt(time.substring(0, 2));
+//             int m = Integer.parseInt(time.substring(3));
+//             minutes[i] = h * 60 + m;
+//         }
+
+//         // sort times in ascending order
+//         Arrays.sort(minutes);
+
+//         // find minimum difference across adjacent elements
+//         int ans = Integer.MAX_VALUE;
+//         for (int i = 0; i < minutes.length - 1; i++) {
+//             ans = Math.min(ans, minutes[i + 1] - minutes[i]);
+//         }
+
+//         // consider difference between last and first element
+//         return Math.min(
+//             ans,
+//             24 * 60 +  minutes[0] - minutes[minutes.length - 1] 
+//         );
+//     }
+// }
+
+// m-2 optimize tc by using bucket sort
+class Solution {
+
+    public int findMinDifference(List<String> timePoints) {
+        // convert input to minutes
+        boolean[] mins = new boolean[1440];
+        for (String time : timePoints){
+            int h = Integer.parseInt(time.substring(0, 2));
+            int m = Integer.parseInt(time.substring(3));
+             int minutes = h * 60 + m;
+
+             // Aagar already true to dono same ho jayega to unka diff 0 hoga
+            if(mins[minutes] == true) return 0;
+            mins[minutes] = true;
+        }
+
+        // find minimum difference across adjacent elements
+        int prev = -1;
+        int firstVal = -1;
+        int minDiff = Integer.MAX_VALUE;
+        for (int curr = 0; curr < 1440; curr++) {
+            if(mins[curr]){ // true hai
+                if(prev == -1){
+                    firstVal = curr;
+                    prev = curr;
+                }else{
+
+                    minDiff = Math.min(minDiff, curr - prev);
+                    prev = curr;
+                }
+            }
+        }
+         // consider difference between last and first element
+        if(prev != -1){
+            minDiff = Math.min(minDiff, 1440 + firstVal - prev);
+        }
+
+       
+       return minDiff;
+    }
+}
+
