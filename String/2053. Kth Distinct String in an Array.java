b39e99c807fb3956fadfe34b992d1fca37042e47
Lalit Kumar <shahulalit5@gmail.com>
2026-02-09 13:55:19 +0530

    2053. Kth Distinct String in an Array

---
+++ String/2053. Kth Distinct String in an Array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class 2053. Kth Distinct String in an Array {
+  class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer>map = new HashMap<>();
+
+        for(String s : arr){
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+         int count = 0;
+        for(String s : arr){
+            if(map.get(s) == 1){
+                count++;
+
+                if(count == k) return s;
+            }
+        }
+        return "";
+    }
+}
+}
