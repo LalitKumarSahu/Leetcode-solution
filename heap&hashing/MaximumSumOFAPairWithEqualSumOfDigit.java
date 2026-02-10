56afcd664bd9ad44e8d04f501036b72199d72952
Lalit Kumar <shahulalit5@gmail.com>
2026-02-10 10:18:10 +0530

    maximum sum of a pair with equal sum of a digit

---
+++ heap&hashing/MaximumSumOFAPairWithEqualSumOfDigit.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+
+public class MaximumSumOFAPairWithEqualSumOfDigit {
+  class Solution {
+    public int maximumSum(int[] nums) {
+        HashMap<Integer, Integer>map = new HashMap<>();
+        int ans = -1;
+        for(int num : nums){
+            int digitSum = getDigitSum(num);
+            if(map.containsKey(digitSum)){
+                int prev = map.get(digitSum);
+                ans = Math.max(ans, prev + num);
+                map.put(digitSum, Math.max(prev, num));
+            }else{
+                map.put(digitSum, num);
+            }
+        }
+        return ans;
+       
+    }
+    public int getDigitSum(int num){
+       int sum = 0;
+           while(num > 0){
+            sum += (num%10); // last digit
+            num /= 10; // last digit remove
+           } 
+           return sum;    
+    }
+}
+
+class Solution {
+    // 10^9 - 1 = 999999999 = 81 is the digit sum
+    public int maximumSum(int[] nums) {
+        // HashMap<Integer, Integer>map = new HashMap<>();
+        int map[] = new int[82];
+        int ans = -1;
+        for(int num : nums){
+            int digitSum = getDigitSum(num);
+            if(map[digitSum] > 0){
+                // int prev = map.get(digitSum);
+                int prev = map[digitSum];
+                ans = Math.max(ans, prev + num);
+                map[digitSum] =  Math.max(prev, num);
+                // map.put(digitSum, Math.max(prev, num));
+            }else{
+                // map.put(digitSum, num);
+                map[digitSum] = num;
+            }
+        }
+        return ans;
+       
+    }
+    public int getDigitSum(int num){
+       int sum = 0;
+           while(num > 0){
+            sum += (num%10); // last digit
+            num /= 10; // last digit remove
+           } 
+           return sum;    
+    }
+}
+}
