2d4f11142041056a99448015c8f241f1551cfebb
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-02-12 11:05:01 +0530

    Time: 7 ms (99.59%) | Memory: 46.1 MB (95.23%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int freq[]  = new int[3];
+        int count= 0;
+        int currCount = 0;
+        int left = 0;
+
+        int n = s.length();
+
+        for(int right = 0; right < n; right++){
+             
+             // expanding phase
+            char ch = s.charAt(right);
+            freq[ch - 'a']++;
+            if(freq[ch - 'a'] == 1){
+                currCount++;
+            }
+
+            // shrinking
+            while(currCount == 3){
+                count += (n - right);
+                char ch2 = s.charAt(left);
+                freq[ch2 - 'a']--;
+                if(freq[ch2 - 'a'] == 0){
+                    currCount--;
+                }
+                left++;
+            }
+        }
+        return count;
+    }
+}
