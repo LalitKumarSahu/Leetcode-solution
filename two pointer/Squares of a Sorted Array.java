bc6629ab7b3f74ce4733bb2c9cb662766f29a9e2
Lalit Kumar <shahulalit5@gmail.com>
2026-02-13 12:47:03 +0530

    m-2 in sq of a sorted array

--- two pointer/Squares of a Sorted Array.java
+++ two pointer/Squares of a Sorted Array.java
@@ -13,4 +13,33 @@ public class Squares of a Sorted Array {
         return nums;
     }
 }
+   
+// m-2
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        int res[] = new int[n];
+        int j = n-1;
+        while(left <= right){
+            int val1 = nums[left] * nums[left];
+            int val2 = nums[right] * nums[right];
+
+            if(val1 >= val2){
+                res[j] = val1;
+                left++;
+                j--;
+            }else{
+                res[j] = val2;
+                j--;
+                right--;
+            }
+        }
+        return res;
+       
+    }
+}
+
 }
