169b51ac0e824de4ede30f93e15a24b43e0ab00f
Lalit Kumar <shahulalit5@gmail.com>
2026-02-14 08:52:19 +0530

    m-2 seprate white and black

--- two pointer/SeparateBlackAndWhiteBalls.java
+++ two pointer/SeparateBlackAndWhiteBalls.java
@@ -25,4 +25,27 @@ class Solution {
     return count;
     }
 }
+
+// m-2 using 2 pointer
+// agar curr black ball hai to continue cur++ 
+// agr cuur white ball hai to find the count and inc the last
+// last ye track krega ki iske piche sab white ball hai
+// curr == black to curr++
+// jab curr = white to count = curr - last and last += 1
+class Solution {
+    public long minimumSteps(String s) {
+        int n = s.length();
+        int last = 0;        // ye track karega ki next 0 ko kis index par hona chahiye
+        long swapCount = 0;  // total moves
+        
+        for (int curr = 0; curr < n; curr++) {
+            if (s.charAt(curr) == '0') {
+                // agar '0' mila, to usko left side (index = last) pe lana hai
+                swapCount += (curr - last);  
+                last++;  // agla 0 next position pe jayega
+            }
+        }
+        return swapCount;
+    }
+}
 }
