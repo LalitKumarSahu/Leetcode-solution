7840f1f077d7eb0b2ffd685c6d5b90d6047c4d9d
Lalit Kumar <shahulalit5@gmail.com>
2026-02-14 08:03:39 +0530

    sort color

---
+++ two pointer/SortColors.java
@@ -0,0 +1,32 @@
+public class SortColors {
+  class Solution {
+    // create a array and start se 0 dalenge aur end se 2, 1 hoga to skip 
+    // start se end 1 dal denge
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int i = 0;
+        
+        while (i <= end) {
+            if (nums[i] == 0) {
+                swap(nums, i, start);
+                i++;
+                start++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, end);
+                end--;  // Don't increment `i`, as the new value at `i` needs to be checked
+                
+            } else { // nums[i] == 1
+                i++;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+}
