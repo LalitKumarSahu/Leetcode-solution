e98d84feb1b9c1ec55e0221c08ce91ff34013f35
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-02-15 09:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (16.19%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,16 +1,43 @@
+// class Solution {
+//     public int maximumCount(int[] nums) {
+//         int pcount = 0;
+//         int ncount = 0;
+
+//         for(int num : nums){
+//             if(num > 0){
+//                 pcount++;
+//             } else if(num < 0){
+//                 ncount++;
+//             }
+//         }
+
+//         return Math.max(pcount, ncount);
+//     }
+// }
+
 class Solution {
     public int maximumCount(int[] nums) {
-        int pcount = 0;
-        int ncount = 0;
+       //negpos >-1 --> first zero ka idx
+       //pospos >0 --->first postive ka idx
+       int  n = nums.length;
+       int negPos = binarySearch(nums, 0, n-1, -1);
+       int posPos = binarySearch(nums, negPos, n-1, 0);
+       int posPosCount = (n - posPos);
 
-        for(int num : nums){
-            if(num > 0){
-                pcount++;
-            } else if(num < 0){
-                ncount++;
+       return Math.max(negPos, posPosCount); 
+
+    }
+
+    public int binarySearch(int nums[], int start, int end, int target){
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] <= target){
+                start = mid + 1;
+            }else{
+                end = mid -1;
             }
         }
-
-        return Math.max(pcount, ncount);
+        return start;
     }
 }
