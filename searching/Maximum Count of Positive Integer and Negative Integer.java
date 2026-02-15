0d559167304b149f2af3a2276c28226a9d9782ea
Lalit Kumar <shahulalit5@gmail.com>
2026-02-15 09:52:56 +0530

    Maximum Count of Positive Integer and Negative Integer using binary search

--- searching/Maximum Count of Positive Integer and Negative Integer.java
+++ searching/Maximum Count of Positive Integer and Negative Integer.java
@@ -16,4 +16,32 @@ public class Maximum Count of Positive Integer and Negative Integer {
     }
 }
 
+
+// m-2
+class Solution {
+    public int maximumCount(int[] nums) {
+       //negpos >-1 --> first zero ka idx
+       //pospos >0 --->first postive ka idx
+       int  n = nums.length;
+       int negPos = binarySearch(nums, 0, n-1, -1);
+       int posPos = binarySearch(nums, negPos, n-1, 0);
+       int posPosCount = (n - posPos);
+
+       return Math.max(negPos, posPosCount); 
+
+    }
+
+    public int binarySearch(int nums[], int start, int end, int target){
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] <= target){
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }
+        }
+        return start;
+    }
+}
 }
