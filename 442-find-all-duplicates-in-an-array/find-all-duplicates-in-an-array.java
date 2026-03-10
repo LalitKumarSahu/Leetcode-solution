1b24b35a612347a3ed7fe011ff9ca83eca814f60
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-03-10 07:09:32 +0530

    Time: 6 ms (90.39%) | Memory: 57.7 MB (80.71%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -0,0 +1,67 @@
+// //Time: O(n)-using map
+// //Space: O(n) (extra space for map)
+
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+//         Map<Integer, Integer> freq = new HashMap<>();
+//         List<Integer> result = new ArrayList<>();
+
+//         for (int num : nums) {
+//             freq.put(num, freq.getOrDefault(num, 0) + 1);
+//         }
+
+//         // Iterate using keySet
+//         for (Integer key : freq.keySet()) {
+//             if (freq.get(key) == 2) {
+//                 result.add(key);
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
+// //tc:O(N), sc:O(1) using pop
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+        
+//         List<Integer> result = new ArrayList<>();
+//         for(int i = 0; i<nums.length; i++){
+//             int element = Math.abs(nums[i]);
+//             int seat = element - 1;
+//             if(nums[seat] < 0){ // duplicate
+//                 result.add(element);
+//             }else{
+//                 nums[seat] = -nums[seat];
+//             }
+//         }
+       
+
+//         return result;
+//     }
+// }
+
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+       
+       int n = nums.length;
+       List<Integer>res = new ArrayList<>();
+       for(int i = 0; i<n; i++){
+        int elm = Math.abs(nums[i]);
+
+        int seat = elm - 1;
+
+        if(nums[seat] < 0){
+            res.add(elm);
+        }else{
+            nums[seat] = -nums[seat];
+        }
+       }
+       return res;
+    }
+}
+
+
+
+
