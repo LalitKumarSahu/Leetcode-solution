ae93bf2b29e2c4c3c6e3c4ee7acdd351dcf27970
Lalit Kumar <shahulalit5@gmail.com>
2026-03-28 14:44:17 +0530

    patching array

--- Arrays/patchingArray.java
+++ Arrays/patchingArray.java
@@ -1,56 +1,26 @@
-public class patchingArray {
-  class Solution {
-    public int minPatches(int[] nums, int n) {
-        
-        long sumFormed = 0;
-        long expectedSum = 1;// ye hai 1 to n
-        int minPatches = 0;
-        int i= 0;
-        int m = nums.length;
-        while(sumFormed < n){
-            // matlab vha tak ban gaya hai 
-            if(sumFormed >= expectedSum){
-                expectedSum = sumFormed + 1; // check for the next value
-            }else{
-                // expected sum > sumformed
-                // to mai nums[i] vale elm ko include kar sakta hun
-              if(i<m && nums[i] <= expectedSum){
-                sumFormed += nums[i];
-                i++;
-              }else{
-                 minPatches++;
-                 sumFormed += expectedSum;
-              }
-            }
-        }
-        return minPatches;
-    }
-}
-
-// m-2
 class Solution {
     public int minPatches(int[] nums, int n) {
-         long sumFormed = 0;
-         long expectedSum = 1;
-         int minPatches = 0;
-         int i = 0;
-         int m = nums.length;
+        long sumFormed = 0;
+        long expectedSum = 1;
+        int minPatch = 0;
+        int i = 0;
+        int m = nums.length;
 
-         while( sumFormed < n){
-            if(sumFormed >= expectedSum){
-              expectedSum = sumFormed + 1;
-            }else{
-                if(i<m && nums[i] <= expectedSum){
-                    sumFormed += nums[i];
-                    i++;
-                }else{
-                    minPatches++;
-                    sumFormed += expectedSum;
-                }
+        while (sumFormed < n) {
+
+            if (i < m && nums[i] <= expectedSum) {
+                // use array element
+                sumFormed += nums[i];
+                i++;
+            } else {
+                // add patch
+                minPatch++;
+                sumFormed += expectedSum;
             }
-         }
-         return minPatches;
 
+            expectedSum = sumFormed + 1;
+        }
+
+        return minPatch;
     }
-}
 }
