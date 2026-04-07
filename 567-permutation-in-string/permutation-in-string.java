19447dcb32c685d3365da395e29c8f9b5d7c64ee
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-04-07 18:34:35 +0530

    Time: 6 ms (87.82%) | Memory: 43.9 MB (79.60%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,39 +1,35 @@
-class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        if(n > m) return false;
-        int map1[] = new int[26];
+// class Solution {
+//     public boolean checkInclusion(String s1, String s2) {
+//         int n = s1.length();
+//         int m = s2.length();
+//         if(n > m) return false;
+//         int map1[] = new int[26];
 
-        for(int i = 0; i<n; i++){
-            map1[s1.charAt(i) - 'a']++;
-        }
-        // i = window ka starting index
-        for(int i = 0; i<=m-n; i++){
-            int map2[] = new int[26];
+//         for(int i = 0; i<n; i++){
+//             map1[s1.charAt(i) - 'a']++;
+//         }
+//         // i = window ka starting index
+//         for(int i = 0; i<=m-n; i++){
+//             int map2[] = new int[26];
 
-            // size of the s1 j → window ke andar ka offset (0 se n-1 tak)
-            for(int j = 0; j<n; j++){
-                map2[s2.charAt(i+j) - 'a']++; // 0 - "e", 1-"i" =>"ei"
-            }
-            if(isMatched(map1, map2)){
-                return true;
-            }
-        }
-        return false;
-    }
-    private boolean isMatched(int map1[], int map2[]){
-        for(int i = 0; i<26; i++){
-            if(map1[i] != map2[i]) return false;
-        }
-        return true;
+//             // size of the s1 j → window ke andar ka offset (0 se n-1 tak)
+//             for(int j = 0; j<n; j++){
+//                 map2[s2.charAt(i+j) - 'a']++; // 0 - "e", 1-"i" =>"ei"
+//             }
+//             if(isMatched(map1, map2)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     private boolean isMatched(int map1[], int map2[]){
+//         for(int i = 0; i<26; i++){
+//             if(map1[i] != map2[i]) return false;
+//         }
+//         return true;
 
-    }
-}
-
-// m-2 sliding window 
-
-
+//     }
+// }
 
 
 // class Solution {
@@ -96,13 +92,40 @@ class Solution {
 //        return false;
 
 //     }
-//     public boolean isMatched(int map1[], int map2[]){
-//       for(int i = 0; i<26; i++){
-//         if(map1[i] != map2[i]){
-//              return false;
-//         }
 
-//       }
-//       return true; 
-//       }
-// }
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if(n > m) return false;
+        int map1[] = new int[26];
+        int map2[] = new int[26];
+        for(int i = 0; i<n; i++){
+            map1[s1.charAt(i) - 'a']++;
+            map2[s2.charAt(i) - 'a']++;
+        }
+        // init window
+         if(isMatched(map1, map2)){
+            return true;
+      }
+        for(int i = 1; i<=m-n; i++){
+            map2[s2.charAt(i-1) - 'a']--; // del
+            map2[s2.charAt(i+n -1) - 'a']++; //add
+            
+            if(isMatched(map1, map2)){
+            return true;
+        }
+
+        }
+        return false;
+    }
+    public boolean isMatched(int map1[], int map2[]){
+      for(int i = 0; i<26; i++){
+        if(map1[i] != map2[i]){
+             return false;
+        }
+
+      }
+      return true; 
+      }
+}
