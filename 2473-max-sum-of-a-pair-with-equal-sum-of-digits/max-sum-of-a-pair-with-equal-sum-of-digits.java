3a1ddece421b7d68d03df8c05b1bd5b2296cae44
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-04-09 20:47:37 +0530

    Time: 45 ms (61.24%) | Memory: 90.2 MB (49.10%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,58 +1,30 @@
-// class Solution {
-//     public int maximumSum(int[] nums) {
-//         HashMap<Integer, Integer>map = new HashMap<>();
-//         int ans = -1;
-//         for(int num : nums){
-//             int digitSum = getDigitSum(num);
-//             if(map.containsKey(digitSum)){
-//                 int prev = map.get(digitSum);
-//                 ans = Math.max(ans, prev + num);
-//                 map.put(digitSum, Math.max(prev, num));
-//             }else{
-//                 map.put(digitSum, num);
-//             }
-//         }
-//         return ans;
-       
-//     }
-//     public int getDigitSum(int num){
-//        int sum = 0;
-//            while(num > 0){
-//             sum += (num%10); // last digit
-//             num /= 10; // last digit remove
-//            } 
-//            return sum;    
-//     }
-// }
+// brute force
+// digit sum ko map me store kar lenge her digit sum ke liye maximum 2 value ka sum find karenge
 
+
+// m-2 optimize instead of storing all value in array store only 2 value 
 class Solution {
-    // 10^9 - 1 = 999999999 = 81 is the digit sum
     public int maximumSum(int[] nums) {
-        // HashMap<Integer, Integer>map = new HashMap<>();
-        int map[] = new int[82];
+       HashMap<Integer,Integer>map = new HashMap<>();
         int ans = -1;
         for(int num : nums){
             int digitSum = getDigitSum(num);
-            if(map[digitSum] > 0){
-                // int prev = map.get(digitSum);
-                int prev = map[digitSum];
-                ans = Math.max(ans, prev + num);
-                map[digitSum] =  Math.max(prev, num);
-                // map.put(digitSum, Math.max(prev, num));
+            if(map.containsKey(digitSum)){
+                int prevNum = map.get(digitSum);
+                ans = Math.max(ans, prevNum + num);
+                map.put(digitSum, Math.max(prevNum, num));
             }else{
-                // map.put(digitSum, num);
-                map[digitSum] = num;
+                map.put(digitSum, num);
             }
         }
         return ans;
-       
     }
     public int getDigitSum(int num){
-       int sum = 0;
-           while(num > 0){
-            sum += (num%10); // last digit
-            num /= 10; // last digit remove
-           } 
-           return sum;    
+        int sum = 0;
+        while(num >0){
+            sum += (num % 10); // extract last digit
+            num /= 10; // remove last digit
+        }
+        return sum;
     }
 }
