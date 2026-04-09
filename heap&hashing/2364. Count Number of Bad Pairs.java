99f0237b09403e624c643de000ef8c9a34b70da5
Lalit Kumar <shahulalit5@gmail.com>
2026-04-09 21:23:29 +0530

    count bad pair optimize soln

--- heap&hashing/2364. Count Number of Bad Pairs.java
+++ heap&hashing/2364. Count Number of Bad Pairs.java
@@ -15,4 +15,24 @@ public class 2364. Count Number of Bad Pairs {
         
     }
 }
+// m-2 by using hashMap
+
+    public long countBadPairs2(int[] nums) {
+      
+        long n = nums.length;
+        long goodPair = 0;
+        long totalPair = n * (n-1)/2;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            int val = i - nums[i];
+            int valCount = map.getOrDefault(val, 0);
+            goodPair += valCount;
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        
+        return totalPair - goodPair;
+    }
+
+
+
 }
