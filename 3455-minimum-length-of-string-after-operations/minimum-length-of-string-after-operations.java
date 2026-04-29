935b1e2145e3725b441fd7c004d95f4d096b26f2
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-04-29 13:28:14 +0530

    Time: 9 ms (97.35%) | Memory: 48.5 MB (54.50%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,17 +1,40 @@
+// class Solution {
+//     public int minimumLength(String s) {
+//         HashMap<Character,Integer>map = new HashMap<>();
+//         int n = s.length();
+//         int count = 0;
+//         for(int i =0; i<n; i++){
+//             char ch = s.charAt(i);
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+//         }
+//         for(int val : map.values()){
+//             if(val % 2 == 0){
+//                 count += 2;
+//             }else{
+//                 count += 1;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int minimumLength(String s) {
-        HashMap<Character,Integer>map = new HashMap<>();
+        // HashMap<Character,Integer>map = new HashMap<>();
+        int freq[] = new int[26];
         int n = s.length();
         int count = 0;
         for(int i =0; i<n; i++){
             char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+             freq[ch-'a']++;
         }
-        for(int val : map.values()){
-            if(val % 2 == 0){
-                count += 2;
-            }else{
-                count += 1;
+        for(int i = 0; i<26; i++){
+            if(freq[i] != 0){
+                if(freq[i] % 2 == 0){
+                    count += 2;
+                }else{
+                    count += 1;
+                }
             }
         }
         return count;
