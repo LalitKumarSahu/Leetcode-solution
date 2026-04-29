9c656d60efb56e81a0d0eb3939ebcd0240bf752b
Lalit Kumar <shahulalit5@gmail.com>
2026-04-29 16:06:46 +0530

    add 1593 split a string into the max number of unique substring

---
+++ String/1593. Split a String Into the Max Number of Unique Substrings.java
@@ -0,0 +1,60 @@
+public class 1593. Split a String Into the Max Number of Unique Substrings {
+  // class Solution {
+//     //tc:O(N * 2^N) , sc-n substring
+//     int maxCount;
+//     public int maxUniqueSplit(String s) {
+//         maxCount = 0;
+//         HashSet<String>set = new HashSet<>();
+//         backtrack(s, set, 0);
+//         return maxCount;
+//     }
+//     // generating all posibility substring  = 2^n
+//     // every posiblity ke liye n option hai
+//     public void backtrack(String s, HashSet<String>set, int idx){
+//         int n = s.length();
+//         if(idx == n){
+//             maxCount = Math.max(maxCount, set.size());
+//             return;
+//         }
+//         for(int i = idx; i<n; i++){
+//             String sub = s.substring(idx, i+1);
+//             if(!set.contains(sub)){
+//                 set.add(sub);
+//                 backtrack(s, set, i+1);
+//                 set.remove(sub);
+//             }
+//         }
+//     }
+// }
+
+class Solution {
+    int maxCount;
+    public int maxUniqueSplit(String s) {
+        maxCount = 0;
+        HashSet<String>set = new HashSet<>();
+        backtrack(s, set, 0);
+        return maxCount;
+     
+  }
+  public void backtrack(String s, HashSet<String>set, int idx){
+        int n  = s.length();
+        if(idx == n){
+            maxCount = Math.max(maxCount, set.size());
+            return;
+        }
+        // ababccc = a, ab, aba, abab, ababc,ababcc,ababccc
+        //b, ba, bab, babc,babcc,babccc
+        //ab, abc,abcc,abccc
+        //c, cc,ccc
+        // ish type me substring banegi
+        for(int i = idx; i < n; i++){
+            String sub = s.substring(idx, i+1);
+            if(!set.contains(sub)){
+                set.add(sub);
+                backtrack(s, set, i+1);
+                set.remove(sub);
+            }
+        }
+  }
+}
+}
