2e4dcf35fa590cc7c5f0d75d279cc296cbd3794e
Lalit Kumar <shahulalit5@gmail.com>
2026-05-01 13:54:19 +0530

    1497 check if array pairs are divisible by k

---
+++ heap&hashing/1497. Check If Array Pairs Are Divisible by k.java
@@ -0,0 +1,43 @@
+public class 1497. Check If Array Pairs Are Divisible by k {
+  // class Solution {
+//     public boolean canArrange(int[] arr, int k) {
+//         int[] map = new int[k];
+        
+//         for (int elm : arr) { // n
+//             int rem = ((elm % k) + k) % k;
+//             map[rem]++;
+//         }
+        
+//         // Check if count of remainder 0 is even
+//         if (map[0] % 2 != 0) return false;
+
+//         // Check other remainders
+//         for (int rem = 1; rem < k; rem++) {  // k/2
+//             int comp = k - rem;
+//             if (map[rem] != map[comp]) return false;
+//         }
+
+//         return true;
+//     }
+// }
+
+//-------->
+class Solution2 {
+    public boolean canArrange(int[] arr, int k) {
+        int map[] = new int[k];
+
+        for(int elm : arr){
+            int rem = ((elm % k) + k) % k;
+            map[rem]++;
+        }
+        if(map[0] % 2 != 0) return false;
+
+        for(int rem =1 ; rem <= k/2; rem++){
+            int com = k - rem;
+
+            if(map[rem] != map[com]) return false;
+        }
+        return true;
+    }
+ }
+}
