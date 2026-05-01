ee754b757100070952da668ebb8a8bc1fe2b2feb
Lalit Kumar <shahulalit5@gmail.com>
2026-05-01 13:58:40 +0530

    divide palyer into teams of equal skill

---
+++ heap&hashing/2491. Divide Players Into Teams of Equal Skill.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+public class 2491. Divide Players Into Teams of Equal Skill {
+  class Solution {
+    public long dividePlayers(int[] skill) {
+        int n = skill.length;
+        int team = n/2;
+        int totalSum = 0;
+        HashMap<Integer, Integer>map = new HashMap<>();
+
+        for(int elm : skill){
+            map.put(elm, map.getOrDefault(elm, 0) +1);
+            totalSum += elm;
+        }
+        if(totalSum % team != 0) return -1;
+
+        int targetSum = totalSum/team;
+        long res = 0;
+
+        for(int elm : skill){
+            if(map.get(elm) == 0){
+                continue;
+            }
+            map.put(elm, map.get(elm)-1);
+            int partner = targetSum - elm;
+            if(!map.containsKey(partner) || map.get(partner) == 0){
+                return -1;
+            }
+            map.put(partner, map.get(partner)-1);
+            res = res + ((long)elm * (long)partner);
+
+
+        }
+        return res;
+    }
+}
+}
