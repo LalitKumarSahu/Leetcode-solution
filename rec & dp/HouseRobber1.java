c51b5e9577a9c12f159d8905057b320176f4f0d6
Lalit Kumar <shahulalit5@gmail.com>
2026-05-03 14:44:43 +0530

    bottom up approach

--- rec & dp/HouseRobber1.java
+++ rec & dp/HouseRobber1.java
@@ -48,4 +48,22 @@ class Solution2 {
        return dp[i];
     }
 }
+//m-3 by tabulation
+class Solution3 {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        int[] dp = new int[n + 1]; // Use n+1 to align with robUtil's index
+         dp[0] = 0;
+         dp[1] = nums[0];
+         
+         for(int i = 2; i < n+1; i++ ){
+            int pick = nums[i - 1] + dp[i-2];
+            int noPick = 0 + dp[i-1];
+             dp[i] = Math.max(pick, noPick);
+         }
+         return dp[n];
+    }
+
+}
 }
