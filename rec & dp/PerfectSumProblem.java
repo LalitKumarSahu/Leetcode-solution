4c88864318e8653ac8dd4a9af620e685739dfd2a
Lalit Kumar <shahulalit5@gmail.com>
2026-05-03 17:54:08 +0530

    using memo

--- rec & dp/PerfectSumProblem.java
+++ rec & dp/PerfectSumProblem.java
@@ -28,4 +28,49 @@ public class PerfectSumProblem {
         return pick + noPick;
     }
 }
+  
+class Solution2 {
+    // Function to calculate the number of subsets with a given sum
+    public int perfectSum(int[] nums, int target) {
+        int n = nums.length;
+        int dp[][] = new int[n][target+1];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<target+1; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return perfectSum(nums, target, n-1,dp);
+        
+    }
+    public int perfectSum(int[] nums, int target, int idx, int dp[][]) {
+        if(idx == 0){
+            if(target == 0 && nums[0] == 0){
+                dp[idx][target] = 2;
+                return 2;
+            }
+            else if(target == 0) {
+                dp[idx][target] = 1;
+                return 1;
+            }
+            else if(target == nums[idx]){
+                 dp[idx][target] = 1;
+                 return 1;
+            }
+            else {
+                dp[idx][target] = 0;
+                return 0;
+            }
+        }
+        if(dp[idx][target] != -1){
+            return dp[idx][target];
+        }
+        int pick = 0;
+        if(nums[idx] <= target){
+            pick = perfectSum(nums, target-nums[idx], idx-1, dp);
+        }
+        int noPick = perfectSum(nums, target, idx-1, dp);
+        dp[idx][target] = pick + noPick;
+        return dp[idx][target];
+    }
+}
 }
