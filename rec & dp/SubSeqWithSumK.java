33b50d6c97393c8cdcfed018a7763e9c5e6e2ed5
Lalit Kumar <shahulalit5@gmail.com>
2026-05-03 16:13:42 +0530

    bottom to top  sub seq with k sum

--- rec & dp/SubSeqWithSumK.java
+++ rec & dp/SubSeqWithSumK.java
@@ -66,4 +66,42 @@ class Solution2 {
         return noPick;
     }
 }
+// using bottom to top
+
+class Solution3 {
+    public boolean checkSubsequenceSum(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        int dp[][] = new int[n][k+1]; //(idx, target)
+        // base case : k == 0 hai to koi bhi idx hot true hoga
+        for(int i = 0; i<n; i++){
+            dp[i][0] = 1;
+        }
+        // idx == 0 hai to target equal to arr[0] to true otherwise false
+        for(int t = 1; t <= k; t++){
+            if(t == arr[0]){
+                dp[0][t] = 1;
+            }else{
+                dp[0][t] = 2;
+            }
+        }
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<=k; j++){
+                
+            int pick = 2;
+            if(arr[i] <= j){
+                pick = dp[i-1][j - arr[i]];//rec(arr, j - arr[i], i-1, dp);
+                if(pick == 1){
+                     dp[i][j] = 1;
+                    continue;
+                }
+            }
+            int noPick = dp[i-1][j];//rec(arr, j, i-1, dp);
+             dp[i][j] = noPick;
+            }
+        }
+        return (dp[n-1][k] == 1);
+    }
+}
+ 
 }
