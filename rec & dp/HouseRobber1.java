c9e14e15cdeac7fcf653341c58b101b9ffeb5c08
Lalit Kumar <shahulalit5@gmail.com>
2026-05-03 14:36:31 +0530

    added top to down method to solve this

--- rec & dp/HouseRobber1.java
+++ rec & dp/HouseRobber1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class HouseRobber1 {
   class Solution {
     public int rob(int[] nums) {
@@ -15,6 +17,35 @@ public class HouseRobber1 {
 
         return Math.max(pick, noPick);
 
+    }
+}
+  
+// top to bottom
+class Solution2 {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        int[] dp = new int[n+1];  // shifting logic bcz array can not hold negative idx
+        Arrays.fill(dp, -1);
+        return robUtil(nums, n, dp);
+    } 
+    public int robUtil(int[] nums, int i, int[] dp) {
+         if (i == 1){ // i == 0
+            dp[i] = nums[0];
+            return nums[0];
+        }
+        if (i == 0) {
+            dp[i] = 0;
+            return 0;
+        }
+       if(dp[i] != -1){
+        return dp[i];
+       }
+       int pick = nums[i-1] + robUtil(nums, i-2, dp);
+       int noPick = 0 + robUtil(nums, i-1, dp);
+
+       dp[i] = Math.max(pick, noPick);
+       return dp[i];
     }
 }
 }
