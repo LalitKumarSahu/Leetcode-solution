eed42874de3ccbca1dd4fde0e48542d833f545ac
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-05-04 16:30:17 +0530

    Time: 62 ms (24.56%) | Memory: 46.5 MB (25.36%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,20 +1,68 @@
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int lis[] = new int[n];
+//         Arrays.fill(lis, 1);
+//         int maxLen = 1;
+
+//         for(int i = 1; i<n; i++){
+
+//             for(int p = 0; p<i; p++){
+//                 if(nums[p] < nums[i]){
+//                     lis[i] = Math.max(lis[i], 1 + lis[p]);
+//                 }
+//             }
+
+//             if(lis[i] > maxLen){
+//                 maxLen = lis[i];
+//             }
+//         }
+
+//         return maxLen;
+//     }
+// }
+
+// print lis
+// jis idx ki vjha se lis update hota hai ush idx ko prev me store kar lenge
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         int lis[] = new int[n];
-        Arrays.fill(lis, 1);
+        int prev[] = new int[n];
+        for(int i = 0; i<n; i++){
+            lis[i] = 1;
+            prev[i] = i;
+        }
         int maxLen = 1;
+        int maxIdx = 0;
         for(int i = 1; i<n; i++){
 
             for(int p = 0; p<i; p++){
                 if(nums[p] < nums[i]){
-                    lis[i] = Math.max(lis[i], 1 + lis[p]);
+                    if(lis[i] < 1 + lis[p]){
+                       lis[i] =  1 + lis[p];
+                       prev[i] = p;
+                    }
+                    
                 }
             }
+
             if(lis[i] > maxLen){
                 maxLen = lis[i];
+                maxIdx = i;
             }
         }
+        ArrayList<Integer>res = new ArrayList<>();
+        res.add(nums[maxIdx]);
+        while(maxIdx != prev[maxIdx]){
+            maxIdx = prev[maxIdx];
+            res.add(0,nums[maxIdx]);
+        }
+        for(int i = 0; i<res.size(); i++){
+            System.out.print(res.get(i));
+        }
         return maxLen;
+
+       
     }
 }
