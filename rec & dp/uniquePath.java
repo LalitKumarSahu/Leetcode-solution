3337b8de6df1310295f9a3c5eeb4ee0aaee80f8d
Lalit Kumar <shahulalit5@gmail.com>
2026-05-04 16:57:29 +0530

    unique path using bottom up

--- rec & dp/uniquePath.java
+++ rec & dp/uniquePath.java
@@ -47,4 +47,27 @@ public class uniquePath {
     }
 
  }
+ 
+// m-3 by using bottom up approach
+ class Solution3 {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m+1][n+1];
+
+        for(int i=0; i<m+1; i++){
+            for(int j=0; j<n+1; j++){
+                if( i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }else if(i == 1 && j == 1){
+                    dp[i][j] = 1;
+                }else{
+                    int w1 = dp[i-1][j];
+                    int w2 = dp[i][j-1];
+                    dp[i][j] = w1 + w2;
+                }
+            }
+        }
+        return dp[m][n];   
+    }
+   
+ }
 }
