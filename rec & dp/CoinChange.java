63a7fbc9d51e736f735643e8e64b63adabf8685d
Lalit Kumar <shahulalit5@gmail.com>
2026-05-04 12:58:42 +0530

    m-3 bottom up with tabulation

--- rec & dp/CoinChange.java
+++ rec & dp/CoinChange.java
@@ -58,3 +58,37 @@ class Solution2 {
         return dp[idx][amount];
     }
 }
+
+// m-3 bottom up with tabulation
+class Solution3 {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int dp[][] = new int[n][amount+1];
+        // base case idx = 0, for amount
+        for(int a = 0; a <amount+1; a++){
+           if(a % coins[0] == 0){
+             dp[0][a] = a / coins[0];
+           }else{
+           dp[0][a] = (int)(1e9);
+           }
+        }
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<amount+1; j++){
+            int pick = (int)(1e9);
+            if(j >= coins[i]){
+                pick = 1 + dp[i][j-coins[i]];//rec(coins, j - coins[i], idx, dp);
+            }
+            int noPick = dp[i-1][j];//rec(coins, j, i-1, dp);
+
+            dp[i][j] =  Math.min(pick, noPick);     
+            }
+        }
+        if(dp[n-1][amount] == (int)(1e9)){
+            return -1;
+        }
+        return dp[n-1][amount];
+       
+        
+    }
+   
+}
