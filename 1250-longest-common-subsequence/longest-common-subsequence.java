94f02ba5542cd435f607f2d75cef73ee899f12ed
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-05-04 14:55:02 +0530

    Time: 24 ms (31.84%) | Memory: 54.1 MB (35.28%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -22,6 +22,40 @@
 // }
 
 //m-2 top down with memoization
+// class Solution {
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int n = text1.length();
+//         int m = text2.length();
+//         int dp[][] = new int[n+1][m+1];
+//         for(int i = 0; i<n+1; i++){
+//             Arrays.fill(dp[i], -1);
+//         }
+//         return rec(n-1, m-1, text1, text2,dp);
+
+//     }
+//     public int rec(int i , int j,String text1, String text2, int dp[][]){
+//         // base case
+//         if( i < 0 || j < 0){
+
+//             return 0;
+//         }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+//         if(text1.charAt(i) == text2.charAt(j)){
+//             dp[i][j] =  1 + rec(i-1, j-1, text1, text2, dp);
+//             return dp[i][j];
+//         }else{
+//             int case1 = rec(i-1, j, text1, text2, dp);
+//             int case2 = rec(i, j-1, text1, text2, dp);
+//             dp[i][j] =  Math.max(case1, case2);
+//             return dp[i][j];
+//         }
+
+//     }
+// }
+
+// m-3 bottom to top
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
@@ -30,18 +64,19 @@ class Solution {
         for(int i = 0; i<n+1; i++){
             Arrays.fill(dp[i], -1);
         }
-        return rec(n-1, m-1, text1, text2,dp);
+        return rec(n, m, text1, text2,dp);
 
     }
     public int rec(int i , int j,String text1, String text2, int dp[][]){
         // base case
-        if( i < 0 || j < 0){
+        if( i == 0 || j == 0){
+
             return 0;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        if(text1.charAt(i) == text2.charAt(j)){
+        if(text1.charAt(i-1) == text2.charAt(j-1)){
             dp[i][j] =  1 + rec(i-1, j-1, text1, text2, dp);
             return dp[i][j];
         }else{
