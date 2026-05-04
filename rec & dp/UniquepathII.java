e7d265f6d24b41abeacb2c28628e43bdd142aa51
Lalit Kumar <shahulalit5@gmail.com>
2026-05-04 17:05:11 +0530

    unique path 2

---
+++ rec & dp/UniquepathII.java
@@ -0,0 +1,36 @@
+public class UniquepathII {
+  class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        return uniquePaths(m, n, obstacleGrid);
+    }
+
+    public int uniquePaths(int m, int n, int[][] obstacleGrid) {
+        int prev[] = new int[n+1];
+
+        for(int i=1; i<m+1; i++){
+        int curr[] = new int[n+1];
+         
+            for(int j=0; j<n+1; j++){
+                if( i == 0 || j == 0 ||obstacleGrid[i-1][j-1] == 1 ){
+                    curr[j] = 0;
+                }else if(i == 1 && j == 1){
+                    curr[j] = 1;
+                }else{
+                    int w1 = prev[j];
+                    int w2 = curr[j-1];
+                    curr[j] = w1 + w2;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n];   
+    }
+   
+ }
+
+
+
+}
