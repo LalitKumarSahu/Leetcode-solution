012bc94c7d705f1e871968fa005e73251a0d4547
Lalit Kumar <shahulalit5@gmail.com>
2026-05-04 16:30:56 +0530

    print LIS

--- rec & dp/LongestIncreasingSubSeq.java
+++ rec & dp/LongestIncreasingSubSeq.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LongestIncreasingSubSeq {
@@ -22,6 +23,51 @@ public class LongestIncreasingSubSeq {
         }
         
         return maxLen;
+    }
+}
+// printing longest increasing subsequence
+// print lis
+// jis idx ki vjha se lis update hota hai ush idx ko prev me store kar lenge
+class Solution2 {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int lis[] = new int[n];
+        int prev[] = new int[n];
+        for(int i = 0; i<n; i++){
+            lis[i] = 1;
+            prev[i] = i;
+        }
+        int maxLen = 1;
+        int maxIdx = 0;
+        for(int i = 1; i<n; i++){
+
+            for(int p = 0; p<i; p++){
+                if(nums[p] < nums[i]){
+                    if(lis[i] < 1 + lis[p]){
+                       lis[i] =  1 + lis[p];
+                       prev[i] = p;
+                    }
+                    
+                }
+            }
+
+            if(lis[i] > maxLen){
+                maxLen = lis[i];
+                maxIdx = i;
+            }
+        }
+        ArrayList<Integer>res = new ArrayList<>();
+        res.add(nums[maxIdx]);
+        while(maxIdx != prev[maxIdx]){
+            maxIdx = prev[maxIdx];
+            res.add(0,nums[maxIdx]);
+        }
+        for(int i = 0; i<res.size(); i++){
+            System.out.print(res.get(i));
+        }
+        return maxLen;
+
+       
     }
 }
 }
