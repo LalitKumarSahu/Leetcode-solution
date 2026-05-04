bee243ed572db16cf5ed7f08262da399e1200310
Lalit Kumar <shahulalit5@gmail.com>
2026-05-04 16:56:09 +0530

    unique path using top dwon

--- rec & dp/uniquePath.java
+++ rec & dp/uniquePath.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class uniquePath {
   class Solution {
     // tc-O(2^N+M)
@@ -15,4 +17,34 @@ public class uniquePath {
          
     }
 }
+// m-2 by using top down approach
+ class Solution2 {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m+1][n+1];
+        for(int i=0; i<m+1; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return gridWay(m,n,dp);   
+    }
+    public int gridWay(int m, int n, int dp[][]){
+        if(m == 1 && n == 1) {
+            dp[m][n] = 1;
+            return 1;
+        }
+        if( m == 0  || n == 0) {
+            dp[m][n] = 0;
+            return 0;
+        }
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+
+        int w1 = gridWay( m-1, n, dp);
+        int w2 = gridWay( m, n-1, dp);
+          dp[m][n] =  w1 + w2;
+          return w1 + w2;
+         
+    }
+
+ }
 }
