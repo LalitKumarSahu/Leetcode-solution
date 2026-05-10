bbd7f59061bde9e8df4479f851681fe50be432c5
Lalit Kumar <shahulalit5@gmail.com>
2026-05-10 17:50:13 +0530

    1863 sum of all subset xor totalby using xor

--- rec & dp/1863. Sum of All Subset XOR Totals.java
+++ rec & dp/1863. Sum of All Subset XOR Totals.java
@@ -12,3 +12,23 @@ class Solution {
 
     }
 }
+//----m-2--------------------------->
+// number of set bit dekh lo
+//5 =0101
+//1 = 0001
+//6 = 0110
+//--or-------->
+//0111 and then dive by 2^n/2 = 8/2 = 4
+//4*4 + 2*4 + 1 * 4 = 28
+//---------------------------------------->
+class Solution2 {
+    public int subsetXORSum(int[] nums) {
+        int n = nums.length;
+        int totalXor = 0;
+        for(int elm : nums){
+            totalXor = totalXor | elm;
+        }
+        return totalXor * (int)Math.pow(2, n-1); // totalxor << n-1; //left shift by n-1
+    }
+   
+}
