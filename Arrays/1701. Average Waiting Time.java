e5c7813a0998161410df4998549c039353471b50
Lalit Kumar <shahulalit5@gmail.com>
2026-05-23 22:30:04 +0530

    added arrival time

--- Arrays/1701. Average Waiting Time.java
+++ Arrays/1701. Average Waiting Time.java
@@ -18,4 +18,51 @@ class Solution {
 
         return (double) totalWtTime / customers.length;
     }
+
+  // class Solution {
+//     public double averageWaitingTime(int[][] customers) {
+//         int idealTime = 0;
+//         long totalWtTime = 0;
+
+//         for (int[] customer : customers) {
+//             int arrival = customer[0];
+//             int cook = customer[1];
+
+//             if (idealTime <= arrival) {
+//                 idealTime = arrival + cook;
+//                 totalWtTime += cook;
+//             } else {
+//                 idealTime += cook;
+//                 totalWtTime += (idealTime - arrival);
+//             }
+//         }
+
+//         return (double) totalWtTime / customers.length;
+//     }
+// }
+
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        int idealTime = 0;
+        long totalWtTime = 0;
+
+        for (int[] customer : customers) {
+            int arrival = customer[0];
+            int cook = customer[1];
+
+            if (idealTime <= arrival) {
+                idealTime = arrival + cook;
+               
+            } else {
+                idealTime += cook;
+              
+            }
+            totalWtTime += (idealTime - arrival);
+        }
+
+        return (double) totalWtTime / customers.length;
+    }
+}
+
+
 }
