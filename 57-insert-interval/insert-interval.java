af50e5da50e36a94fee50f0e790d179d739fcd4c
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-06-16 06:36:41 +0530

    Time: 1 ms (97.98%) | Memory: 46.9 MB (88.13%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,4 +1,3 @@
- // tc-O(N)--iterate 0th to N & and merge in O(1)
 // class Solution {
 //     public int[][] insert(int[][] intervals, int[] newInterval) {
 //         ArrayList<int[]>res = new ArrayList<>();
@@ -28,66 +27,61 @@
 
 // class Solution {
 //     public int[][] insert(int[][] intervals, int[] newInterval) {
-//        ArrayList<int[]>res = new ArrayList<>();
-//        int i = 0;
-//        int n = intervals.length;
-
-//        // nonOverlapping hai newInterval to add into the res
-//        while(i < n  && intervals[i][1] < newInterval[0]){
-//              res.add(intervals[i]);
-//              i++;
-//        }
-
-//        // if overlaping any
-//        while(i < n &&  newInterval[1] >= intervals[i][0]){
+//         ArrayList<int[]>res = new ArrayList<>();
+//         int i = 0;
+//         int n = intervals.length;
+        
+//         //non-overlaping codn
+//         while(i < n && intervals[i][1] < newInterval[0]){
+//             res.add(intervals[i]);
+//             i++;
+//         }
+//         // merging overlapping if any
+//         while(i < n && newInterval[1] >= intervals[i][0]){
 //             newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
 //             newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
 //             i++;
 
-//        }
-//        res.add(newInterval);
+//         }
+//         res.add(newInterval);
 
-//        // insert overlaping and merge them after that interval is persent
-
-//        while(i < n){
-//         res.add(intervals[i]);
-//         i++;
-//        }
-       
-//        return res.toArray(new int[res.size()][]);
-
-
+//          // remaning intervals
+//         while(i < n){
+//             res.add(intervals[i]);
+//             i++;
+//         }
+//         return res.toArray(new int[res.size()][]);
+        
 //     }
 // }
-
-
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
       ArrayList<int[]>res = new ArrayList<>();
       int i = 0;
       int n = intervals.length;
 
-      // nonOverlaping
-      while(i < n && intervals[i][1] < newInterval[0] ){
+      // non overlaping codn add direct into res
+      while(i < n && intervals[i][1] < newInterval[0]){
         res.add(intervals[i]);
         i++;
       }
 
-      // overlaping
-      while(i < n && intervals[i][0] <= newInterval[1]){
-        newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-        newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+
+      // overlaping codn -> start ka min and and end ka max 
+      while( i < n && newInterval[1] >= intervals[i][0]){
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
         i++;
 
       }
       res.add(newInterval);
-     
-     // merge if any
-     while(i < n){
+
+      //append the remaining
+      while(i < n){
         res.add(intervals[i]);
         i++;
-     }
-     return res.toArray(new int[res.size()][]);
-
+      }
+        
+        return res.toArray(new int[res.size()][]);
     }
 }
