6d8cbbc883f47461446900fb1e8497d1e0983b0c
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2026-08-20 22:06:40 +0530

    Time: 2 ms (100.00%) | Memory: 95.2 MB (36.61%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -20,26 +20,49 @@
 //     }
 // }
 
+// class Solution {
+//     public double averageWaitingTime(int[][] customers) {
+//         int idealTime = 0;
+//         long totalWtTime = 0;
+
+//         for (int[] customer : customers) {
+//             int arrival = customer[0];
+//             int cook = customer[1];
+
+//             if (idealTime <= arrival) {
+//                 // itne time chef free hoga
+//                 idealTime = arrival + cook;
+               
+//             } else {
+//                 idealTime += cook;
+              
+//             }
+//             totalWtTime += (idealTime - arrival);
+//         }
+
+//         return (double) totalWtTime / customers.length;
+//     }
+// }
+
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        int idealTime = 0;
-        long totalWtTime = 0;
+        int idealTime = 1;
+        long totalWt = 0;
 
-        for (int[] customer : customers) {
-            int arrival = customer[0];
-            int cook = customer[1];
+        for(int cust[] : customers){
+            int At = cust[0];
+            int FP = cust[1];
 
-            if (idealTime <= arrival) {
-                idealTime = arrival + cook;
-               
-            } else {
-                idealTime += cook;
-              
+            if(idealTime <= At){
+                idealTime = At + FP;
+
+            }else{
+                idealTime += FP;
             }
-            totalWtTime += (idealTime - arrival);
+            totalWt += (idealTime - At);
         }
-
-        return (double) totalWtTime / customers.length;
+        return (double)totalWt / customers.length;
     }
 }
+
 
